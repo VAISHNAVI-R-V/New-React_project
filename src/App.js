6aa60081cd65a80c274d24d87c7334f3105f614b
vaishnavi <vaishnavi.v@perfios.com>
2023-03-25 18:56:06 +0530

    ADDED : Initial Expenses Iteams.

--- src/App.js
+++ src/App.js
@@ -24,9 +24,6 @@ function App() {
   <ExpenseItem title= {expenses[0].title} amount= {expenses[0].amount} date={expenses[0].date} ></ExpenseItem>
   <ExpenseItem title= {expenses[1].title} amount= {expenses[1].amount} date={expenses[1].date}></ExpenseItem>
   <ExpenseItem title= {expenses[2].title} amount= {expenses[2].amount} date={expenses[2].date}></ExpenseItem>
-  {/* <ExpenseItem></ExpenseItem> */}
-  {/* <ExpenseItem></ExpenseItem> */}
-
   </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -7,16 +7,10 @@ function ExpenseItem(props) {
     return( 
     <div className="expense-item">
     <div> {expenseDate.toUTCString()} </div>
-    {/* <div> {expenseDate.toUTCString()} </div> */}
-
-    {/* <div>{expenseDate()}</div> */}
     <div className='expense-item__description'> 
-    {/* <h2> {Math.random()}</h2> */}
     <h2> {expenseTitle}</h2>
 
     <div className='expense-item__price'> {expenseAmount} </div>
-    {/* <h2>Expenses Items List! </h2>
-    <div className="">Name</div> */}
     </div>
     </div> 
     )
